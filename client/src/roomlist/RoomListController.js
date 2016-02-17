fb78a92f16447f31fb243e19cfb2a4d70e8c1428
Quang Nguyen <Kwa@Quangs-MacBook-Pro.local>
2016-02-17 22:24:30 +0000

    login

--- client/src/roomlist/RoomListController.js
+++ client/src/roomlist/RoomListController.js
@@ -1,4 +1,16 @@
-angular.module("chatApp").controller("RoomListController",
-	function RoomListController(){
-		//$scope.roomlist sækja roomlist í bakendann
-	});
+"use strict";
+
+angular.module("chatApp").controller("RoomListController", ["$scope", "socket",
+function RoomListController($scope, socket) {
+	$scope.testvar = "bla";
+	socket.emit("rooms");
+
+	var funcToBeCalledWhenRommlistChanges = function(roomlist) {
+		console.log(roomlist);
+		$scope.$apply(function() {
+			$scope.roomlist = roomlist;
+		});
+	}
+
+	socket.on("roomlist", funcToBeCalledWhenRommlistChanges);
+}]);
