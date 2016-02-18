0fc192a29182eafd8646c46e40e5684331a482da
Quang Nguyen <Kwa@Quangs-MacBook-Pro.local>
2016-02-18 11:03:50 +0000

    adfsaff

--- client/src/roomlist/RoomListController.js
+++ client/src/roomlist/RoomListController.js
@@ -5,11 +5,16 @@ function RoomListController($scope, socket) {
 	$scope.testvar = "bla";
 	socket.emit("rooms");
 
+	$scope.createRoom = function createRoom() {
+		console.log("create room");
+	}
+
 	var funcToBeCalledWhenRommlistChanges = function(roomlist) {
+		console.log("herro");
 		console.log(roomlist);
-		$scope.$apply(function() {
+		//$scope.apply(function() {
 			$scope.roomlist = roomlist;
-		});
+		//});
 	}
 
 	socket.on("roomlist", funcToBeCalledWhenRommlistChanges);
